8f5b527ce94e7ed1d3661838ffa04a0cfdb8a2c2
Deepanshu Chaid <bantar117@gmail.com>
2026-04-08 14:25:08 +0530

    feat: basic cli structure with init command

--- Stage-1/cli-go/main.go
+++ Stage-1/cli-go/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
 
 func main() {
-	fmt.Println("HELLOW")
+	var rootCmd = &cobra.Command{
+		Use: "cogito",
+		Short: "Cogito is a persistent memory layer for Ai",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Welcome to Cogito. Use --help for commands")
+		},
+	}
+
+	var initCmd = &cobra.Command{
+		Use: "init",
+		Short: "Initialize a new Cogito Memo",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Cogito initialized!")
+		},
+	}
+
+	rootCmd.AddCommand(initCmd)
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println("Error initing the library: ", err.Error())
+		os.Exit(1)
+	}
 }
